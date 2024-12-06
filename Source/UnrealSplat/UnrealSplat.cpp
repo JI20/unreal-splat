316f2497e6428fdcfa81d379de037b2f2aae9936
JI20 <jonas.itt@tum.de>
2024-12-06 20:07:49 +0100

    Hello World

---
+++ Source/UnrealSplat/UnrealSplat.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealSplat.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealSplat, "UnrealSplat" );
