dbb208bd183afe8dacf98dee3d747c579a888a01
JI20 <jonas.itt@tum.de>
2024-12-14 17:01:46 +0100

    Implement basis for ply parsing using miniply

--- Source/UnrealSplat/Private/Parser.cpp
+++ Source/UnrealSplat/Private/Parser.cpp
@@ -2,36 +2,73 @@
 
 
 #include "Parser.h"
+#include "Miniply.h"
 #include "HAL/PlatformFileManager.h" // Core
 #include "Misc/FileHelper.h" // Core
 #include "Misc/Paths.h" // Core
 
 FString UParser::ParseFilePLY(FString FilePath, bool& bOutSuccess, FString& OutInfoMessage) {
+	const char* kFileTypes[] = {
+	  "ascii",
+	  "binary_little_endian",
+	  "binary_big_endian",
+	};
+
+	const char* kPropertyTypes[] = {
+	  "char",
+	  "uchar",
+	  "short",
+	  "ushort",
+	  "int",
+	  "uint",
+	  "float",
+	  "double",
+	};
 	
-	// ---- File Reading ----
+	// ---- PLY Parsing ----
 
-	FString AbsolutePath = FPaths::ProjectContentDir() + FilePath;
-	// 
-	if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*AbsolutePath))
-	{
+	FString AbsolutePath = FPaths::ProjectContentDir() + "Models/" + FilePath;
+	FString HeaderLog = "";
+	FString Output = "---- Parsing PLY File ----\n\n";
+	
+	miniply::PLYReader reader(TCHAR_TO_ANSI(*AbsolutePath));
+	
+	if (!reader.valid()) {
 		bOutSuccess = false;
-		OutInfoMessage = FString::Printf(TEXT("Reading String failed - File does not exist - %s"), *AbsolutePath);
-		return "";
+		OutInfoMessage = FString::Printf(TEXT("Parsing PLY failed - Not a valid PLY file - %s"), *AbsolutePath);
+		Output = OutInfoMessage;
+		return Output;
 	}
 
-	FString RetString = "";
-	if (!FFileHelper::LoadFileToString(RetString, *AbsolutePath))
-	{
-		bOutSuccess = false;
-		OutInfoMessage = FString::Printf(TEXT("Reading String failed - Was not able to read the file - %s"), *AbsolutePath);
-		return "";
+	// ---- Content: Parse Header of file ----
+	HeaderLog += "ply\n";
+	HeaderLog += FString::Printf(TEXT("format %s %d.%d\n"), ANSI_TO_TCHAR(kFileTypes[int(reader.file_type())]),
+		reader.version_major(), reader.version_minor());
+	for (uint32_t i = 0, endI = reader.num_elements(); i < endI; i++) {
+		const miniply::PLYElement* elem = reader.get_element(i);
+		HeaderLog += FString::Printf(TEXT("element %s %u\n"), ANSI_TO_TCHAR(elem->name.c_str()), elem->count);
+
+		for (const miniply::PLYProperty& prop : elem->properties) {
+			if (prop.countType != miniply::PLYPropertyType::None) {
+				HeaderLog += FString::Printf(TEXT("property list %s %s %s\n"), ANSI_TO_TCHAR(kPropertyTypes[uint32_t(prop.countType)]),
+					ANSI_TO_TCHAR(kPropertyTypes[uint32_t(prop.type)]), ANSI_TO_TCHAR(prop.name.c_str()));
+			}
+			else {
+				HeaderLog += FString::Printf(TEXT("property %s %s\n"), ANSI_TO_TCHAR(kPropertyTypes[uint32_t(prop.type)]), ANSI_TO_TCHAR(prop.name.c_str()));
+			}
+		}
 	}
+	HeaderLog += "end_header\n\n";
+
+	// ---- Finish up ----
 
 	bOutSuccess = true;
-	OutInfoMessage = FString::Printf(TEXT("Reading String succeeded - %s"), *AbsolutePath);
+	OutInfoMessage = FString::Printf(TEXT("Successfully parsed PLY File - %s\n\n-- PLY Header --\n\n"), *AbsolutePath);
+	Output += OutInfoMessage;
+	Output += HeaderLog;
+	Output += "-- End of PLY Header --\n\n";
+	Output += "---- Finished Parsing PLY File ----";
 
-	// ---- Content Parsing ----
-
-	return RetString;
+	return Output;
 }
 
--- Source/UnrealSplat/Public/Parser.h
+++ Source/UnrealSplat/Public/Parser.h
@@ -7,7 +7,54 @@
 #include "Parser.generated.h"
 
 /**
- * 
+ * Represents parsed data for a single splat, loaded from a regular PLY file.
+ */
+USTRUCT(BlueprintType)
+struct FGaussianSplatData
+{
+
+	GENERATED_BODY()
+
+	// Splat position (x, y, z)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector Position;
+
+	// Normal vectors [optional] (nx, ny, nz)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector Normal;
+
+	// Splat orientation coming as wxyz from PLY (rot_0, rot_1, rot_2, rot_3)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FQuat Orientation;
+
+	// Splat scale (scale_0, scale_1, scale_2)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector Scale;
+
+	// Splat opacity (opacity)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Opacity;
+
+	// Spherical Harmonics coefficients - Zero order (f_dc_0, f_dc_1, f_dc_2)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector ZeroOrderHarmonicsCoefficients;
+
+	// Spherical Harmonics coefficients - High order (f_rest_0, ..., f_rest_44)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FVector> HighOrderHarmonicsCoefficients;
+
+	FGaussianSplatData()
+		: Position(FVector::ZeroVector)
+		, Normal(FVector::ZeroVector)
+		, Orientation(FQuat::Identity)
+		, Scale(FVector::OneVector)
+		, Opacity(0)
+	{
+	}
+};
+
+/**
+ * Class for reading and parsing 3D Splat models into Unreal
  */
 UCLASS()
 class UNREALSPLAT_API UParser : public UBlueprintFunctionLibrary
